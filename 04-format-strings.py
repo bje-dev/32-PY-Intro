c761a69f4a9832b97460f9277cf595b46b5312e4
Bernardo Espinoza <bernardo.julian.espinoza@gmail.com>
2024-02-29 15:31:18 -0300

    Methods string

--- 04-format-strings.py
+++ 04-format-strings.py
@@ -1,5 +1,6 @@
 name = "Bernardo"
 surname = "Espinoza"
 full_name = name + "" + surname
-format_string = f"{name[0]} {2+5}"
+format_string = f"{name[0]} {2 + 5}"
 print(full_name, format_string)
+
---
+++ 05-methods-strings.py
@@ -0,0 +1,20 @@
+name = "bernardo"
+lastname = " julian "
+surname = "ESPINOZA"
+
+print(name.upper())
+print(surname.lower())
+print(lastname.strip().capitalize())
+print(lastname.strip())
+print(lastname.lstrip())
+print(lastname.rstrip())
+print(lastname.find("jul"))
+print(lastname.replace("juli","ju"))
+print("jul" in lastname)
+print("jul" not in lastname)
+
+
+
+
+
+
