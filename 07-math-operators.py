ca02e2d32ab4b935995dec6dafb6d4457e81db05
Bernardo Espinoza <bernardo.julian.espinoza@gmail.com>
2024-03-01 11:58:19 -0300

    Math operators

---
+++ 07-math-operators.py
@@ -0,0 +1,18 @@
+number = 2 #integer
+decimal = 1.2 #float
+imagine =2 + 2j #math function
+
+number += 5
+number *= 5
+number -= 5
+number /= 5
+
+print(1+3)
+print(1-3)
+print(1*3)
+print(1/3)#return float
+print(1//3)#return integer
+print(8%3) #return module
+print(2**3)#return squared
+
+
