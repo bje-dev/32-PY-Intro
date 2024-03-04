019110925018d94f3bb6667a29b638b579dbebdd
Bernardo Espinoza <bernardo.julian.espinoza@gmail.com>
2024-03-04 16:48:43 -0300

    Logical operators

---
+++ 13-logical-operators.py
@@ -0,0 +1,19 @@
+#AND OR NOT
+switch1 = True
+switch2 = False
+switch3 = True
+
+if switch1 and switch2:
+    print("Light on")
+
+if switch1 or switch2:
+    print("Light on")
+
+if not switch1 or switch2:
+    print("Light on")
+
+if switch1 and switch2 and switch3:
+    print("Light on")
+
+#Python has short circuit
+
